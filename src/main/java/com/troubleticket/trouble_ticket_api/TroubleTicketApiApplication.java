09bdd4b12850fa3b5411b0b444c992513c83ae79
macwlu97 <maciej.wnuklipinski@gmail.com>
2026-07-09 01:41:53 +0200

    init

---
+++ src/main/java/com/troubleticket/trouble_ticket_api/TroubleTicketApiApplication.java
@@ -0,0 +1,13 @@
+package com.troubleticket.trouble_ticket_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TroubleTicketApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TroubleTicketApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/troubleticket/trouble_ticket_api/TroubleTicketApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.troubleticket.trouble_ticket_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TroubleTicketApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
